00f383bffe27e8c17158c77348dc67fa7745267b
Rameal Nabeeh <ironman3093@yahoo.com>
2015-04-12 22:10:54 -0700

    Changed the file viewList with comments on how the loop should works but still needs to be worked on syntactically for the proper output

--- src/edu/asu/se/helpers/ViewList.java
+++ src/edu/asu/se/helpers/ViewList.java
@@ -12,6 +12,25 @@ public class ViewList implements ICommand {
 		
 		HttpSession session = request.getSession();
 		
+		PatientDAO hold = new PatientDAO();
+		
+		for(int i = 0; i < PatientList; i++)
+		{
+			String uname = request.getParameter("uname");
+			String pwd = request.getParameter("pwd");
+			String fname = request.getParameter("fname");
+			String lname = request.getParameter("lname");
+			String gender = request.getParameter("gender");
+			String email = request.getParameter("email");
+			String mobileNumber = request.getParameter("mobilenumber");
+			String address = request.getParameter("address");
+			String zipCode = request.getParameter("zipcode");
+			String age=request.getParameter("age");
+			
+			//print the names or give them to a front end service
+		}
+		//the above looks as though you're getting parameters from somewhere then I just want to output them and cycle to the next one in the list
+		
 		
 	}
 }
