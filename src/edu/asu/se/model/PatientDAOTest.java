7726537ebc2e09d0f96a2a7798f5b460fcb1974a
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-04-15 00:47:39 -0700

    Created base unit test file PatientDAO

---
+++ src/edu/asu/se/model/PatientDAOTest.java
@@ -0,0 +1,79 @@
+package edu.asu.se.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PatientDAOTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAddPatient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPatientInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPatientString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddESASRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddBodyPainInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMedicalHistory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAppointment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetInpatientList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEditPatient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPatient() {
+		fail("Not yet implemented");
+	}
+
+}
